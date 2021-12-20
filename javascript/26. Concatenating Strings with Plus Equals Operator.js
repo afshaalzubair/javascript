02ec11c0f4e8edad8853db9211a22e906a6122e7
Afshaal Zubair <afshaalzubair@outlook.com>
2021-12-20 17:44:31 -0600

    initial commit

---
+++ javascript/26. Concatenating Strings with Plus Equals Operator.js
@@ -0,0 +1,9 @@
+// Example
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+// Example 2
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
+
+console.log(myStr);
