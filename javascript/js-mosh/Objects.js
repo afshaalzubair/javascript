697387a2c5db2f4b51a4e7902348c19682799288
Afshaal Zubair <afshaalzubair@outlook.com>
2021-12-25 17:57:21 -0600

    initial commmit

---
+++ javascript/js-mosh/Objects.js
@@ -0,0 +1,14 @@
+
+let person = {
+    name: "Bob",
+    age: 30
+};
+
+// Dot Notation
+person.name = "John";
+
+// Bracket Notation
+let selection = "name";
+person[selection] = "Mary";
+
+console.log(person.name);
