dcea48bea424d84e85886712d5b769f4a3e5fca0
Afshaal Zubair <afshaalzubair@outlook.com>
2021-12-25 17:57:12 -0600

    initial commmit

---
+++ javascript/js-mosh/Dynamic-Typing.js
@@ -0,0 +1,11 @@
+let name = 'John'; // String Literal
+let age = 30; // Number Literal
+let isApproved = false; // Boolean Literal
+let firstName = undefined;
+let selectedColor = null;
+
+console.log(typeof(name));
+console.log(typeof(age));
+console.log(typeof(isApproved));
+console.log(typeof(firstName));
+console.log(typeof(selectedColor));
